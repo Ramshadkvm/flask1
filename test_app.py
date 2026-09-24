5b80edbcdcd09a5024581af0820e1d9c48490bf5
Ramshadkvm <ramshadtkvm@gmail.com>
2026-09-24 12:24:22 +0530

    first commit

---
+++ test_app.py
@@ -0,0 +1,18 @@
+```python
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello, Flask!" in response.data
+```
